0569c421de93f94836136eacd9148d9f20147118
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-28 22:56:26 -0600

    Componente Livewire

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,4 +6,6 @@
     <h2 class="text-xl text-gray-600">Formate online como profesional en tecnologia</h2>
     <h3 class="text-lg text-gray-600">70% de los graduados duplica sus ingresos</h3>
   </div>
+
+  <livewire:course-list>
 @endsection
