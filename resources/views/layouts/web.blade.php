842e6bf43a7b05601e7969db435ebab1ee4b6d1a
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-28 22:13:47 -0600

    index y layout: definiendo estructura HTML

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Cursos de Programacion Web</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <main class="py-10">
+      <div class="container mx-auto px-4">
+        @yield('content')
+      </div>
+    </main>
+  </body>
+</html>
