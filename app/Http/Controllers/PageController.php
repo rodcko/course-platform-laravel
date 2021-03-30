38f25b6f84a6dfcecba4ce74a60ba01627010c17
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-29 22:35:48 -0600

    Detalle del curso

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,8 +12,8 @@ class PageController extends Controller
         return view('index');
     }
 
-    public function course()
+    public function course(Course $course)
     {
-        return view('course');
+        return view('course', compact('course'));
     }
 }
